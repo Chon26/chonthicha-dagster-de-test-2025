54d34590923473a94adca6c3f34884928824841b
Chonthicha Arbsuwan <chonthichaarbsuwan@MacBook-Air-khxng-Chonthicha.local>
2025-04-08 23:26:13 +0700

    add extract data

--- dagster_pipelines/etl/extract.py
+++ dagster_pipelines/etl/extract.py
@@ -2,8 +2,20 @@ import pandas as pd
 
 # 2.1.1 Read KPI evaluation data from the "Data to DB" sheet in the "KPI_FY.xlsm" Excel file
 def read_excel() -> pd.DataFrame:
-    pass
+    df = pd.read_excel("dagster_pipelines/data/KPI_FY.xlsm", sheet_name="Data to DB")
+
+    # ทำการแปลงคอลัมน์ Center_ID เป็น string เพื่อแก้ไขปัญหาการ merge ที่เกิดจากคอลัมน์ Center_ID เป็น object
+    df['Center_ID'] = df['Center_ID'].astype(str)
+    
+    return df
+    
 
 # 2.1.2 Read center master data from the "M_Center.csv" CSV file
 def read_csv() -> pd.DataFrame:
-    pass
+    df = pd.read_csv("dagster_pipelines/data/M_Center.csv")
+
+    # ทำการแปลงคอลัมน์ Center_ID เป็น string เพื่อแก้ไขปัญหาการ merge ที่เกิดจากคอลัมน์ Center_ID เป็น object
+    df['Center_ID'] = df['Center_ID'].astype(str)
+
+    return df
+    
